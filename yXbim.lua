2940f99b3f37b122900eb24834ec8c5fe70f16eb
ddjlb7598 <q18342117598@163.com>
2025-11-15 20:20:08 +0800

    Update and rename AXcnm.lua to yXbim.lua

---
+++ yXbim.lua
@@ -0,0 +1,8 @@
+warn("禁止入侵殺脚本源码")
+warn("你永远拿不到死变态🥱")
+
+local localPlayer = game.Players.LocalPlayer
+local playerName = localPlayer.Name
+
+wait(2)
+game.Players.LocalPlayer:Kick("用户： " .. playerName .. "你好，你这个贪心的家伙")
